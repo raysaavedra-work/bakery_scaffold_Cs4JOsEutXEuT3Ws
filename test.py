f7cefc15e6d3fcc9aaf8a37ac553b6ef8195390a
raysaavedra-work <rayyacosaavedra.work@gmail.com>
2019-09-19 13:49:34 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ class TestAcceptanceStripe(unittest.TestCase):
 
     def test_acceptance_stripe_public_key_has_been_set(self):
         """Check if Stripe key was defined."""
-        pattern = re.compile(r"Stripe\('pk_test_\w{24}'\);", re.I | re.M)
+        pattern = re.compile(r"Stripe\('pk_test_\w+'\);", re.I | re.M)
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'), msg="You didn't define the Stripe key.")
 
@@ -66,6 +66,92 @@ class TestAcceptanceStripe(unittest.TestCase):
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'), msg="You didn't define a cancel URL.")
 
- 
-if __name__ == '__main__':
+
+class AssessmentTestCases(unittest.TestCase):
+    def setUp(self):
+
+        with open("order.html", "r") as file_descriptor:
+            self.dom_str = file_descriptor.read()
+
+        CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+        WINDOW_SIZE = "1920,1080"
+
+        options = selenium.webdriver.ChromeOptions()
+        options.headless = True
+        options.binary_location = '/usr/lib/chromium'
+        options.add_argument("--window-size=%s" % WINDOW_SIZE)
+        options.add_argument("--disable-gpu")
+
+        self.driver = webdriver.Chrome(
+            executable_path=CHROMEDRIVER_PATH, options=options
+        )
+
+    def _get_button_id(self):
+        pattern = re.compile(r"\('checkout-button-sku_\w{14}'\);", re.I | re.M)
+        res = re.search(pattern, self.dom_str)
+        return res.group().split("'")[1]
+
+    def _get_url(self):
+        pattern = re.compile(
+            r"cancelUrl: \'(http|https)://(.*)/order.html\'", re.I | re.M
+        )
+        res = re.search(pattern, self.dom_str)
+        return ":".join(res.group().split(": ")[1:]).strip("'")
+
+    def test_assessment_successful_payment_on_the_checkout_page_redirects_to_order_html(
+        self
+    ):
+        self.driver.get(self._get_url())
+        wait = WebDriverWait(self.driver, 20)
+
+        elem = wait.until(EC.presence_of_element_located((By.ID, self._get_button_id())))
+        elem.click()
+
+        email_elem = wait.until(EC.presence_of_element_located((By.ID, "email")))
+
+        cardnum_elem = self.driver.find_element_by_id("cardNumber")
+        cardexp_elem = self.driver.find_element_by_id("cardExpiry")
+        cardcvc_elem = self.driver.find_element_by_id("cardCvc")
+        cardname_elem = self.driver.find_element_by_id("billingName")
+
+        try:
+            zip_elem = self.driver.find_element_by_id('billingPostalCode')
+        except NoSuchElementException:
+            zip_elem = None
+
+        email_elem.send_keys("assessment@test.com.br")
+        cardnum_elem.send_keys("4242424242424242")
+        cardexp_elem.send_keys("0439")
+        cardcvc_elem.send_keys("424")
+        cardname_elem.send_keys("Selenium Test WebDriver")
+
+        if zip_elem:
+            zip_elem.send_keys('12312')
+
+        confirm_elem = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "SubmitButton--complete")))
+        confirm_elem.click()
+
+        session_id_elem = wait.until(
+            EC.presence_of_element_located((By.ID, "sessionId"))
+        )
+
+        self.assertIn("order_success.html", self.driver.current_url)
+        self.assertTrue(session_id_elem.text)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+    
+
+class TestSubmission(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestSubmission, self).__init__(*args, **kwargs)
+        with open('order.html', 'r') as file_descriptor:
+            self.dom_str = file_descriptor.read()
+
+    
+
+
+if __name__ == "__main__":
     unittest.main()
